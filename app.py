1aa0f1301dc058a6103d68657f91ee32fc71cdd6
PolyLvst <zeerofive05@gmail.com>
2025-06-16 14:43:03 +0700

    separate the chats for each services

--- app.py
+++ app.py
@@ -6,38 +6,42 @@ import importlib
 import os
 from babel.numbers import format_currency
 
-def format_rupiah(value):
-    return format_currency(value, 'IDR', locale='id_ID').replace(",00","")
+class OmziNSEntry:
+    def format_rupiah(value):
+        return format_currency(value, 'IDR', locale='id_ID').replace(",00","")
 
-def load_config(path="config.yml"):
-    if not os.path.exists(path=path):
-        print("-- NO Config yml found --")
-        exit()
-    with open(path, "r", encoding="utf-8") as f:
-        return yaml.safe_load(f)
+    def load_config(path="config.yml"):
+        if not os.path.exists(path=path):
+            print("-- NO Config yml found --")
+            exit()
+        with open(path, "r", encoding="utf-8") as f:
+            return yaml.safe_load(f)
 
-def collect_report():
-    config = load_config()
-    services = config.get("services", [])
-    header = config.get("header")
-    footer = config.get("footer")
-    
-    messages = []
-    total_omzet = Decimal("0")
-    for service in services:
-        try:
-            module = importlib.import_module(service["module"])
-            omzet = module.get_omzet()
-            total_omzet += omzet
-            messages.append(f"{service['name']}:\n└── {format_rupiah(omzet)}")
-        except Exception as e:
-            print(f"⚠️ {service['name']} failed: {e}")
-            messages.append(f"⚠️ {service['name']} failed: CHECK SERVER LOG")
-    
-    joined_messages = '\n'.join(messages)
-    now = datetime.now(tz=pytz.timezone("Asia/Jakarta")).strftime("%d/%m/%Y")
-    report = f"{header}\n\n{joined_messages}\n\n*Total Omzet* : {format_rupiah(total_omzet)}\n\n{footer}\n{now}"
-    return report
+    def collect_report(services_uuid:list[str]):
+        config = OmziNSEntry.load_config()
+        services:list = config.get("services", [])
+        header = config.get("header")
+        footer = config.get("footer")
+        
+        messages = []
+        total_omzet = Decimal("0")
+        for service in services:
+            if service["uuid"] not in services_uuid: continue
+            try:
+                module = importlib.import_module(service["module"])
+                omzet = module.get_omzet()
+                total_omzet += omzet
+                messages.append(f"{service['name']}:\n└── {OmziNSEntry.format_rupiah(omzet)}")
+            except Exception as e:
+                print(f"⚠️ {service['name']} failed: {e}")
+                messages.append(f"⚠️ {service['name']} failed: CHECK SERVER LOG")
+        
+        if len(messages) == 0:
+            messages.append("No configured service --")
+        joined_messages = '\n'.join(messages)
+        now = datetime.now(tz=pytz.timezone("Asia/Jakarta")).strftime("%d/%m/%Y")
+        report = f"{header}\n\n{joined_messages}\n\n*Total Omzet* : {OmziNSEntry.format_rupiah(total_omzet)}\n\n{footer}\n{now}"
+        return report
 
 if __name__ == "__main__":
-    print(collect_report())
+    print(OmziNSEntry.collect_report())
--- bot.py
+++ bot.py
@@ -1,28 +1,51 @@
-from datetime import time, timedelta, timezone
+from datetime import datetime, timedelta, timezone
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-from app import collect_report
+from app import OmziNSEntry
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 BOT_TOKEN = os.environ.get("TOKEN_TELEGRAM_BOT", "")
-CHAT_ID = os.environ.get("CHAT_ID_TELEGRAM", "")
-HOUR_SCHEDULE = int(os.environ.get("HOUR_SCHEDULE", 19))
 WIB = timezone(timedelta(hours=7))
 
-async def send_report(context: ContextTypes.DEFAULT_TYPE):
-    report = collect_report()
-    await context.bot.send_message(chat_id=CHAT_ID, text=report, parse_mode="Markdown")
+class OmziNS:
+    def __init__(self):
+        self.configs = OmziNSEntry.load_config()
+        self.chats = self.configs.get("chats")
+        self.app = Application.builder().token(BOT_TOKEN).build()
+        self.setup_schedules()
 
-async def ping_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("✅ I'm up and running!")
+    def setup_schedules(self):
+        for chat in self.chats:
+            name = chat.get("name")
+            schedule = chat.get("schedule")
+            chat_id = chat.get("chat_id")
+            services = chat.get("services")
+
+            job_parameters = {"chat_id":chat_id, "services_uuid":services}
+            job_time = datetime.strptime(schedule, "%H:%M:%S").time().replace(tzinfo=WIB)
+            # Run daily attach to job queue
+            self.app.job_queue.run_daily(self.send_report, time=job_time, data=job_parameters)
+            # self.app.job_queue.run_repeating(self.send_report, interval=15, first=0, data=job_parameters)
+            print(f"-- Job Queue : {name} Assigned")
+
+    async def send_report(self, context: ContextTypes.DEFAULT_TYPE):
+        parameters = context.job.data
+        services_uuid = parameters["services_uuid"]
+        chat_id = parameters["chat_id"]
+        report = OmziNSEntry.collect_report(services_uuid=services_uuid)
+        await context.bot.send_message(chat_id=chat_id, text=report, parse_mode="Markdown")
+
+    async def ping_command(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text("✅ I'm up and running!")
+    
+    def start(self):
+        # app.add_handler(CommandHandler("report", send_report))
+        self.app.add_handler(CommandHandler("ping", self.ping_command))
+        self.app.run_polling()
 
 if __name__ == "__main__":
     print("-- Bot Started --")
-    app = Application.builder().token(BOT_TOKEN).build()
-    # app.add_handler(CommandHandler("report", send_report))
-    app.add_handler(CommandHandler("ping", ping_command))
-    app.job_queue.run_daily(send_report, time=time(hour=HOUR_SCHEDULE, tzinfo=WIB))
-    # app.job_queue.run_repeating(send_report, interval=5, first=0)
-    app.run_polling()
+    omzins = OmziNS()
+    omzins.start()
